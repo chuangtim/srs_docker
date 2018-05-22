92bbbfb1bb27b384a3b1b230b51e97b754adce3f
chuangtim <zhuangtim@sina.cn>
2018-05-22 18:34:46 +0800

    修改启动脚步

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-basepath=$(cd `dirname $0`; pwd)
-cd basepath
-sudo wget -qO- https://get.docker.com/ | sh
-sudo service docker start
-sudo cp daemon.json /etc/docker/
-sudo docker build srs .
+echo $PWD
+#sudo wget -qO- https://get.docker.com/ | sh
+#sudo service docker start
+sudo cp $PWD/daemon.json /etc/docker/
+sudo chmod a+rwx /etc/docker/daemon.json
+echo /etc/docker/daemon.json
+sudo docker build .
 
 sudo docker run -p 1935:1935 -p 1985:1985 -p 8080:8080 --restart=always srs 
